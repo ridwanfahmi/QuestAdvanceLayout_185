0444225e559207ba4b5b56ee888c1b1f612af964
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-24 09:19:24 +0700

    menambahkan struktur dasar untuk ActivitasPertama screen

---
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -0,0 +1,22 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Coloum(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
+
+@Composable
+fun Coloum(modifier: Modifier, horizontalAlignment: Alignment.Horizontal) {
+    TODO("Not yet implemented")
+}
+
