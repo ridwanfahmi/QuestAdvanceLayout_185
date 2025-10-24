56d5ad8de7bdf94a94e5389ddc293c5bc731b49d
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-24 09:57:02 +0700

    membuat box untuk text copy

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -74,6 +75,10 @@ fun ActivitasPertama(modifier: Modifier){
                         modifier = Modifier.padding(top = 10.dp)
                     )
                 }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
             }
         }
 
