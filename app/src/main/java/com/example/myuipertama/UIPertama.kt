25410a593959b383fd9485e776080f8e71cb5c52
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-24 09:45:19 +0700

    memanggil gambar logo_umy

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,11 +1,20 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -14,7 +23,7 @@ import com.example.myuipertama.R
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Coloum(
+    Column(
         modifier = Modifier
             .padding(top = 100.dp)
             .fillMaxSize(),
@@ -30,19 +39,25 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
-
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray //
+            )
+        ){
+            Row() {
+                val gambar = painterResource( id = R.drawable.logo_umy)
+                Image( //
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size( size = 100.dp)
+                )
+            }
+        }
 
     }
 }
-
-@Composable
-fun Coloum(
-    modifier: Modifier,
-    horizontalAlignment: Alignment.Horizontal,
-    content: @Composable () -> Unit
-) {
-    TODO("Not yet implemented")
-}
-
-
 
