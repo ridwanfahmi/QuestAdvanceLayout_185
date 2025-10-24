d20838295a3b9a789f5bdba2cc453d39c3b19982
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-24 09:39:00 +0700

    memanggil dua variable text prodi dan univ

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,9 +1,16 @@
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myuipertama.R
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
@@ -12,11 +19,30 @@ fun ActivitasPertama(modifier: Modifier){
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height(height = 25.dp))
+
+
+    }
 }
 
 @Composable
-fun Coloum(modifier: Modifier, horizontalAlignment: Alignment.Horizontal) {
+fun Coloum(
+    modifier: Modifier,
+    horizontalAlignment: Alignment.Horizontal,
+    content: @Composable () -> Unit
+) {
     TODO("Not yet implemented")
 }
+
+
 
