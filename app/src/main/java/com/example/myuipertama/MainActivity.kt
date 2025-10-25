c4c7dc062158809f6d95e47e8a2946e47d8b449c
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-25 20:47:58 +0700

    mengganti logo setting

--- app/src/main/java/com/example/myuipertama/MainActivity.kt
+++ app/src/main/java/com/example/myuipertama/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.advancelayout.ActivitasPertama
+import com.example.myuipertama.ui.Profileku
 import com.example.myuipertama.ui.theme.MyuipertamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyuipertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ActivitasPertama(
+                    Profileku(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
